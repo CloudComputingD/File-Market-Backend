3bfab2862741a91e56623401b93e0dc9483c7b3f
dusdjhyeon <dusdj0813@gmail.com>
2023-05-04 13:53:55 +0900

    env configure

---
+++ src/main/java/com/fs/filemarket/FilemarketApplication.java
@@ -0,0 +1,13 @@
+package com.fs.filemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilemarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fs/filemarket/FilemarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs.filemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilemarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
