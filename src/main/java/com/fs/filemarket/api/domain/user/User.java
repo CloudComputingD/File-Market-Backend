46863766e46dea895903a1646fd512f302e8c06e
Sungah <sungah0815@gmail.com>
2023-05-19 00:01:59 +0900

    feat: User - User class 수정

--- src/main/java/com/fs/filemarket/api/domain/user/User.java
+++ src/main/java/com/fs/filemarket/api/domain/user/User.java
@@ -29,6 +29,12 @@ public class User {
         this.nickname = this.nickname == null ? name : this.nickname;
     }
 
+    // JPA 데이터베이스로 저장할 때 Enum 값을 어떤 형태로 저장할지 결정
+    // 기본은 int 로 된 숫자이지만, DB 확인 시 편의성을 위해 문자열로 저장될 수 있도록 선언
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
 //    @Column(nullable = false)
 //    private String login_id;
 //
@@ -36,17 +42,22 @@ public class User {
 //    private String password;
 
     @Builder
-    public User(Integer id, String name, String email, String nickname) {
+    public User(Integer id, String name, String email, String nickname, Role role) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.nickname = nickname;
+        this.role = role;
     }
 
     public User update(String nickname) {
         this.nickname = nickname;
 
         return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
     }
 
 
