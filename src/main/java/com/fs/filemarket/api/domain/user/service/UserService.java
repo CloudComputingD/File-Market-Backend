02e8882f02fbfb844979e728e24a34003d979b2b
Sungah <sungah0815@gmail.com>
2023-05-24 17:38:07 +0900

    style: User - service 수정

--- src/main/java/com/fs/filemarket/api/domain/user/service/UserService.java
+++ src/main/java/com/fs/filemarket/api/domain/user/service/UserService.java
@@ -3,13 +3,31 @@ package com.fs.filemarket.api.domain.user.service;
 import com.fs.filemarket.api.domain.user.Role;
 import com.fs.filemarket.api.domain.user.User;
 import com.fs.filemarket.api.domain.user.dto.UserDTO;
+import com.fs.filemarket.api.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
+@RequiredArgsConstructor
 // 자체 로그인, 회원가입 시 사용하는 화원 가입 API
-public interface UserService {
+public class UserService {
 
-    void join(UserDTO userDTO) throws Exception;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    default User ToEntity(UserDTO userDTO) {
+    public void join(UserDTO userDTO) throws Exception{
+
+        if(userRepository.findByEmail(userDTO.getEmail()).isPresent()) {
+            throw new Exception("이미 존재하는 이메일입니다.");
+        }
+
+        if(userRepository.findByLoginId(userDTO.getLogin_id()).isPresent()) {
+            throw new Exception("이미 존재하는 아이디입니다.");
+        }
+
         User user = User.builder()
                 .name(userDTO.getName())
                 .email(userDTO.getEmail())
@@ -18,8 +36,11 @@ public interface UserService {
                 .role(Role.USER)
                 .build();
 
-        return user;
+        user.passwordEncode(passwordEncoder);
+        userRepository.save(user);
+
     }
+
 
 
 }
