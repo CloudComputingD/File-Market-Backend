5bb763d52b55d3a791b697572307ee9f36f174aa
Sungah <sungah0815@gmail.com>
2023-05-24 16:42:42 +0900

    feat: Configuration - login

---
+++ src/main/java/com/fs/filemarket/api/global/config/login/filter/CustomUsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,68 @@
+package com.fs.filemarket.api.global.config.login.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+
+public class CustomUsernamePasswordAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    /**
+     * 스프링 시큐리티의 폼 기반의 UsernamePasswordAuthenticationFilter를 참고하여 만든 커스텀 필터
+     * 거의 구조가 같고, Type이 Json인 Login만 처리하도록 설정한 부분만 다르다. (커스텀 API용 필터 구현)
+     * Username : 회원 아이디 -> email로 설정
+     * "/login" 요청 왔을 때 JSON 값을 매핑 처리하는 필터
+     */
+
+    private static final String DEFAULT_LOGIN_REQUEST_URL = "/login"; // "/login"으로 오는 요청을 처리
+    private static final String HTTP_METHOD = "POST"; // 로그인 HTTP 메소드는 POST
+    private static final String CONTENT_TYPE = "application/json"; // JSON 타입의 데이터로 오는 로그인 요청만 처리
+    private static final String USERNAME_KEY = "login_id"; // 회원 로그인 시 로그인아이디 요청 JSON Key : "login_id"
+    private static final String PASSWORD_KEY = "password"; // 회원 로그인 시 비밀번호 요청 JSon Key : "password"
+    private static final AntPathRequestMatcher DEFAULT_LOGIN_PATH_REQUEST_MATCHER =
+            new AntPathRequestMatcher(DEFAULT_LOGIN_REQUEST_URL, HTTP_METHOD); // "/login" + POST로 온 요청에 매칭된다.
+
+    private final ObjectMapper objectMapper;
+
+    public CustomUsernamePasswordAuthenticationFilter(ObjectMapper objectMapper) {
+        super(DEFAULT_LOGIN_PATH_REQUEST_MATCHER); // 위에서 설정한 "login" + POST로 온 요청을 처리하기 위해 설정
+        this.objectMapper = objectMapper;
+    }
+
+    /**
+     * 요청 JSON Example
+     * {
+     *    "login_id" : "hi123"
+     *    "password" : "test123"
+     * }
+     */
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException {
+        if(request.getContentType() == null || !request.getContentType().equals(CONTENT_TYPE)  ) {
+            throw new AuthenticationServiceException("Authentication Content-Type not supported: " + request.getContentType());
+        }
+
+        String messageBody = StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8);
+
+        Map<String, String> usernamePasswordMap = objectMapper.readValue(messageBody, Map.class);
+
+        String login_id = usernamePasswordMap.get(USERNAME_KEY);
+        String password = usernamePasswordMap.get(PASSWORD_KEY);
+
+        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(login_id, password);//principal 과 credentials 전달
+
+        return this.getAuthenticationManager().authenticate(authRequest);
+    }
+}
