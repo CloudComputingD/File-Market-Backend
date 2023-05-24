d5017dd714f5b25b78a7f1f17d77bad0c79fc5b3
Sungah <sungah0815@gmail.com>
2023-05-24 16:40:44 +0900

    style: User - service 수정

--- src/main/java/com/fs/filemarket/api/domain/user/service/UserService.java
+++ src/main/java/com/fs/filemarket/api/domain/user/service/UserService.java
@@ -1,4 +1,26 @@
 package com.fs.filemarket.api.domain.user.service;
 
-public class UserService {
+import com.fs.filemarket.api.domain.user.Role;
+import com.fs.filemarket.api.domain.user.User;
+import com.fs.filemarket.api.domain.user.dto.UserDTO;
+
+// 자체 로그인, 회원가입 시 사용하는 화원 가입 API
+public interface UserService {
+
+    void join(UserDTO userDTO) throws Exception;
+
+    default User ToEntity(UserDTO userDTO) {
+        User user = User.builder()
+                .name(userDTO.getName())
+                .email(userDTO.getEmail())
+                .login_id(userDTO.getLogin_id())
+                .password(userDTO.getPassword())
+                .role(Role.USER)
+                .build();
+
+        return user;
+    }
+
+
 }
+
