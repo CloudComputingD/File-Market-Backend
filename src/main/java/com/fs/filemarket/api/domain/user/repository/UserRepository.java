337c4cf8cdb426000b7f4c0b48394d05668a6a55
Sungah <sungah0815@gmail.com>
2023-05-24 16:37:13 +0900

    feat: User - UserRepository 수정

--- src/main/java/com/fs/filemarket/api/domain/user/repository/UserRepository.java
+++ src/main/java/com/fs/filemarket/api/domain/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.fs.filemarket.api.domain.user.repository;
 
+import com.fs.filemarket.api.domain.user.SocialType;
 import com.fs.filemarket.api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     // 데이터베이스에 존재하는 이메일인지 확인하는 method
     Optional<User> findByEmail(String email);
+    Optional<User> findByLoginId(String login_id);
+
+    Optional<User> findByRefreshToken(String refreshToken);
+
+    Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
 }
