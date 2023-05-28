1df9691b3119cd427429b697e071e275b35e1373
Sungah <sungah0815@gmail.com>
2023-05-28 14:58:30 +0900

    feat: User - user package 수정

--- src/main/java/com/fs/filemarket/api/domain/user/repository/UserRepository.java
+++ src/main/java/com/fs/filemarket/api/domain/user/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     // 데이터베이스에 존재하는 이메일인지 확인하는 method
     Optional<User> findByEmail(String email);
