e7dbad72d3a9111cddc8f351c31bb128487e2071
Sungah <sungah0815@gmail.com>
2023-06-09 02:46:46 +0900

    feat: User - UserController에 api 추가

--- src/main/java/com/fs/filemarket/api/domain/user/controller/UserController.java
+++ src/main/java/com/fs/filemarket/api/domain/user/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.fs.filemarket.api.domain.user.controller;
 
+import com.fs.filemarket.api.domain.user.User;
 import com.fs.filemarket.api.domain.user.dto.UserDTO;
 import com.fs.filemarket.api.domain.user.service.UserService;
 import com.mysql.cj.log.Log;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,27 +16,48 @@ public class UserController {
 
     private final UserService userService;
 
+
+    // 서비스 첫 화면
+
 //    @GetMapping("/")
-//    public String Home() {
-//        return "home";
-//    }
-//
-//    @GetMapping("/user/join")
-//    public String createUserForm() {
-//        return "user/createUserForm";
+//    public String index() {
+//        return "index";
 //    }
 
-    @PostMapping("/join")
-    public String join(@RequestBody UserDTO userDTO) throws Exception {
-        userService.join(userDTO);
+    /** 회원가입 **/
 
-        return "회원가입 성공";
+    // 회원가입(sign-up) 화면 요청
+    @GetMapping("/signup")
+    public String signupForm() {
+        return "signupForm";
     }
 
-    @GetMapping("oauth2/join")
-    public String oauth2Join(){
+    // 자체 회원가입
+    @PostMapping("/signup")
+    public String signup(@RequestBody UserDTO userDTO) throws Exception {
+        userService.join(userDTO);
+
+        return "회원가입 성공";  // return "redirect:/index"
+    }
+
+    // oauth2 를 통한 회원가입
+    @GetMapping("oauth2/signup")
+    public String oauth2Signup(){
 
         return "oauth2를 통한 회원가입 성공!!!";
+    }
+
+
+    /** 로그인 **/
+    // 자체 로그인 path -> /login + POST 로 온 요청에 매핑
+    // oauth 로그인 path -> /oauth2/authorization/google 매핑
+
+
+    // 유저 정보 반환
+    @GetMapping("/current")
+    public ResponseEntity<User> getCurrentUser() {
+        User currentUser = userService.getCurrentUser();
+        return ResponseEntity.ok(currentUser);
     }
 
 
